c4d817b864ce1ca973869c9dfdb60b350672de34
margreataru <aravindkumararu12@gmail.com>
2023-08-12 11:00:17 +0530

    modified the script

--- backup.sh
+++ backup.sh
@@ -2,7 +2,5 @@
 backup_dir="/root/Aravind"
 source_dir="/root/Hello"
 timestamp=$(date +%Y%m%d%H%M%S)
-touch "/root/aravind_"${timestamp}".tar.gz"
-cp -r "/root/Aravind" "/root/aravind_"${timestamp}".tar.gz"
 Backupfile="aravind_"${timestamp}".tar.gz"
 tar -cvzf "${backup_dir}/${Backupfile}" "${source_dir}"
