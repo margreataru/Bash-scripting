dc6b0d907c9e7fa9fa53aad1e3be60974540589f
margreataru <aravindkumararu12@gmail.com>
2023-08-12 10:46:12 +0530

    modified the script

--- backup.sh
+++ backup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
-PathDir = "~/Aravind"
-SourceDir = "~/Hello"
-
-timestamp = "%Y%m%d%H%M%S"
-
-Backupfile = "aravind-${timestamp}.tar.gz"
-tar -cvzf "${SourceDir}/${Backupfile}" "${PathDir}"
+backup_dir="/root/Aravind"
+source_dir="/root/Hello"
+timestamp=$(date +%Y%m%d%H%M%S)
+touch "/root/aravind_"${timestamp}".tar.gz"
+cp -r "/root/Aravind" "/root/aravind_"${timestamp}".tar.gz"
+Backupfile="aravind_"${timestamp}".tar.gz"
+tar -cvzf "${backup_dir}/${Backupfile}" "${source_dir}"
